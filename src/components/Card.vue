84e391a0ff73eb402c54368e1017b38cb713517e
maestroDever <dream.big.dev.423@gmail.com>
2020-09-24 00:59:23 +0100

    add initial view

---
+++ src/components/Card.vue
@@ -0,0 +1,45 @@
+<template>
+  <article class="card" :class="{ favorite: image.favourite }">
+    <h1 class="card--title">{{ image.name }}</h1>
+    <figure class="card--image">
+      <img
+        :src="`https://portal-tb.lynxx.co/api-test/image/${image.id}`"
+        :alt="image.name"
+      />
+    </figure>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "ImageCard",
+  props: {
+    image: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  position: relative;
+
+  &--title {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    margin: 0;
+  }
+
+  &--image {
+    margin: 0;
+
+    img {
+      width: 100%;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-  </div>
+  <section class="images-wrapper">
+    <div v-if="loading">Loading...</div>
+    <div class="image" v-for="image in images" :key="image.id">
+      <ImageCard :image="image" />
+    </div>
+  </section>
 </template>
 
 <script>
 // @ is an alias to /src
+import { mapState } from "vuex";
+import ImageCard from "@/components/Card.vue";
 
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    ImageCard
+  },
+  computed: {
+    ...mapState(["images", "loading"])
+  }
 };
 </script>
+
+<style lang="scss" scoped>
+.images-wrapper {
+  line-height: 0;
+  column-count: 4;
+  column-gap: 0px;
+
+  .image {
+    width: 100% !important;
+    height: auto !important;
+  }
+}
+</style>
