5549aa118529812444d785912a85072095d2f398
maestroDever <dream.big.dev.423@gmail.com>
2020-09-24 00:08:24 +0100

    install axios

--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import axios from "axios";
+
 Vue.config.productionTip = false;
+
+const base = axios.create({
+  baseURL: "https://portal-tb.lynxx.co/api-test/"
+});
+
+Vue.prototype.API = base;
 
 new Vue({
   router,
