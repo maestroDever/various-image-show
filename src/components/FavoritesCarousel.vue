d7242d5371110a321f46f0f7d1ef3c575af32d11
maestroDever <dream.big.dev.423@gmail.com>
2020-09-25 10:24:20 +0100

    add FavouritesCarousel component

---
+++ src/components/FavoritesCarousel.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="favorites">
+    <figure class="favorite-image" v-for="image in favorites" :key="image.id">
+      <Card :image="image" :show-star="false" />
+    </figure>
+  </section>
+</template>
+
+<script>
+import Card from "./Card.vue";
+export default {
+  name: "FavoritesGallery",
+  props: {
+    favorites: {
+      type: Array,
+      default: () => []
+    }
+  },
+  components: {
+    Card
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.favorites {
+  padding: 0.5vw;
+  font-size: 0;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  flex-flow: row wrap;
+
+  .favorite-image {
+    -webkit-box-flex: auto;
+    -ms-flex: auto;
+    flex: auto;
+    width: 200px;
+    margin: 0.5vw;
+
+    img {
+      width: 100%;
+      height: auto;
+    }
+  }
+}
+@media screen and (max-width: 400px) {
+  .favorite-image {
+    margin: 0;
+  }
+  .favorites {
+    padding: 0;
+  }
+}
+</style>
